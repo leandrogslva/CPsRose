531d9167167216992268d9aad8c58d771687d64f
Leandro <leandrogslva@gmail.com>
2019-09-27 17:01:53 -0300

    feat: adicionado contatos no top da tela também

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
+        <v-card height="50px" class="text-center pink darken-1">
+            <v-card-text style="color: white">
+                <v-icon color="white">mdi-phone</v-icon>
+                Telefone: (11)5852-9044 /
+                <v-icon color="white">mdi-whatsapp</v-icon>
+                WhatsApp: (11)98393-3692 /
+                <v-icon color="white">mdi-car</v-icon>
+                Frete grátis
+            </v-card-text>
+        </v-card>
         <img src="../imagens/header_wallpaper.jpg" id="header">
-        <v-toolbar  class="pink darken-1" id="">
+        <v-toolbar  class="pink darken-1">
             <template>
                 <v-tabs
                 v-model="tab"
@@ -10,12 +20,13 @@
                 >
                     <v-tabs-slider color="pink lighten-4"></v-tabs-slider>
                     <v-tab v-for="item in menu" :key="item" >
-                            <a style="color: white; text-decoration: none;" :href="'#' + item">
-                                <v-btn text 
-                                large
-                                color="white">
-                                    {{ item }}
-                                </v-btn></a>
+                        <a style="color: white; text-decoration: none;" :href="'#' + item">
+                            <v-btn text 
+                            large
+                            color="white">
+                                {{ item }}
+                            </v-btn>
+                        </a>
                     </v-tab>
                 </v-tabs>
             </template>
@@ -27,7 +38,7 @@ export default {
     data(){
         return{
             tab: null,
-            menu: ['Boticario', 'Natura', 'Hinode', 'Eudora','Contato'],
+            menu: ['Boticario', 'Natura', 'Hinode', 'Eudora', 'Contato'],
         }
     }
 }
@@ -39,9 +50,5 @@ export default {
         padding: 0px;
         margin: 0px;
     }
-    h2{
-        text-align: left;
-        color: purple;
-        margin-top: 100px;
-    }
+    
 </style>
